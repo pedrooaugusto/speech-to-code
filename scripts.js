967ae4b1a6ab0a27776d96fe45477e18f9662940
Pedro Augusto <pedro42augusto@gmail.com>
2021-09-04 12:48:23 -0300

    update manifest version

--- scripts.js
+++ scripts.js
@@ -25,16 +25,19 @@ if (task === 'version') {
 
     if (version === '') return console.log('Specify a version!')
 
+    function updateVersion(pathDoc) {
+        const doc = JSON.parse(fs.readFileSync(pathDoc, 'utf-8'))
+
+        doc.version = version
+
+        fs.writeFileSync(pathDoc, JSON.stringify(doc, null, '\t'))
+    }
+
     const files = ['server', 'client', 'webapp', 'spoken-vscode-driver', 'spoken']
 
-    files.forEach(item => {
-        const package = JSON.parse(fs.readFileSync(path.resolve(item, 'package.json'), 'utf-8'))
+    files.forEach(item => updateVersion(path.resolve(item, 'package.json')))
 
-        package.version = version
-
-        fs.writeFileSync(path.resolve(item, 'package.json'), JSON.stringify(package, null, '\t'))
-
-    })
+    updateVersion(path.resolve('webapp', 'public', 'manifest.json'))
 }
 
 /**
